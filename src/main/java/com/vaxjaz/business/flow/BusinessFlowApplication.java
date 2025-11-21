0f219406fd9974044bf517a067d4b8a27f9ed661
jie.li <jie.li@nx-engine.com>
2025-11-21 10:15:26 +0800

    init

---
+++ src/main/java/com/vaxjaz/business/flow/BusinessFlowApplication.java
@@ -0,0 +1,13 @@
+package com.vaxjaz.business.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vaxjaz/business/flow/BusinessFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaxjaz.business.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
